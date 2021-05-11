37a5293146e40b4ca55d3bf0e3588c5b48566a07
lihsuan <l22761712@gmail.com>
2021-05-11 21:18:43 +0800

    basic version

---
+++ password.py
@@ -0,0 +1,12 @@
+print('請輸入密碼，最多3次')
+
+password='a123456'
+ans=input('請輸入密碼:')
+for i in range(2,0,-1):
+    
+    if ans == password:
+        print('輸入正確!!')
+        break
+    else:
+        ans=input('你還有'+str(i)+'次機會:')
+        i-=1
